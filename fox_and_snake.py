bebcec06734e0dc569e778c38df5af9d06af3326
beamop <beamop@pm.me>
2021-09-11 19:34:39 +0200

    add fox_and_snake

---
+++ fox_and_snake.py
@@ -0,0 +1,13 @@
+def main():
+    n, m = map(int, input().split())
+
+    for x in range(0, n):
+        for y in range(0, m):
+            if (x % 2) == 0:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print("")
+
+if __name__ == '__main__':
+    main()
